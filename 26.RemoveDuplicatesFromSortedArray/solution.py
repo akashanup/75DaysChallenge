7ab5b39924c2637bd43cb8e6b14bc6b7941a02e5
Akash Anup <akashanup.1993@hotmail.com>
2022-03-26 17:14:37 +0530

    26.RemoveDuplicatesFromSortedArray

--- 26.RemoveDuplicatesFromSortedArray/solution.py
+++ 26.RemoveDuplicatesFromSortedArray/solution.py
@@ -1,28 +1,13 @@
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return 1
-        i = 1
-        dupStart, dupEnd = None, None
-        while i < len(nums):
-            if nums[i] == nums[i-1]:
-                # Duplicate Found
-                if dupEnd is None:
-                    dupStart = dupEnd = i
-                else:
-                    dupEnd = i
-            else:
-                while i < len(nums)-1 and nums[i] == nums[i+1]:
-                    # Duplicate Found
-                    if not dupStart:
-                        dupStart = i
-                    dupEnd = i
-                    i += 1
-
-                if dupStart is not None:
-                    # Swap the current element with the element at index dupStart
-                    nums[dupStart], nums[i] = nums[i], nums[dupStart]
-                    dupStart += 1
-            i += 1
-
-        return dupStart if dupStart else len(nums)
+    def removeDuplicates(self, nums):
+        n = len(nums)
+        if n <= 1:
+            return n
+        j = 0
+        for i in range(n-1):
+            if nums[i] != nums[i+1]:
+                nums[j] = nums[i]
+                j += 1
+        nums[j] = nums[n-1]
+        # All the values after index j can be removed.
+        return j+1
