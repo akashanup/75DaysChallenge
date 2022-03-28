a35099167640267d3c177a8b4900f9d1bb0ddabf
Akash Anup <akashanup.1993@hotmail.com>
2022-03-28 19:03:56 +0530

    48.RotateImage

---
+++ 48.RotateImage/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in range(n//2):
+            for j in range(i, n-i-1):
+                temp = matrix[j][n-1-i]
+                matrix[j][n-1-i] = matrix[i][j]
+                matrix[i][j] = matrix[n-1-j][i]
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j]
+                matrix[n-1-i][n-1-j] = temp
