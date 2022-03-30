aa93abed5652c30032586ce2d2efb28717fde46d
Akash Anup <akashanup.1993@hotmail.com>
2022-03-30 23:05:18 +0530

    5.JumpGameII

--- 5.JumpGameII/solution.py
+++ 5.JumpGameII/solution.py
@@ -4,14 +4,14 @@ class Solution:
             return 0
 
         current = farthest = nums[0]
-        steps = 1
+        jumps = 1
         i = 1
         while i < len(nums)-1:
             current -= 1
             farthest -= 1
             farthest = max(farthest, nums[i])
             if current == 0:
-                steps += 1
+                jumps += 1
                 current = farthest
             i += 1
-        return steps
+        return jumps
