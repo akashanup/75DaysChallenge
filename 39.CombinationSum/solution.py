086c3d9282a091d57117510a2f1e75ba0f12a96c
Akash Anup <akashanup.1993@hotmail.com>
2022-03-30 18:20:23 +0530

    39.CombinationSum

---
+++ 39.CombinationSum/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def recur(self, candidates, target, temp, combinations):
+        if target == 0:
+            combinations.add(tuple(sorted(temp)))
+
+        for candidate in candidates:
+            if candidate <= target:
+                self.recur(candidates, target - candidate, temp + [candidate], combinations)
+        return combinations
+
+    def combinationSum(self, candidates: List[int], target: int, combination=[]) -> List[List[int]]:
+        return self.recur(candidates, target, [], set())
