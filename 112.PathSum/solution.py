961b6bf6efb7d474aacf4a4acbc02add398a6c91
Akash Anup <akashanup.1993@hotmail.com>
2022-04-21 11:20:46 +0530

    112.PathSum

---
+++ 112.PathSum/solution.py
@@ -0,0 +1,13 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
+        if not root:
+            return False
+        if not root.left and not root.right:
+            return targetSum == root.val
+        return self.hasPathSum(root.left, targetSum-root.val) or self.hasPathSum(root.right, targetSum-root.val)
