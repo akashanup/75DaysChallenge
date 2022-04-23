abd8104ebc0dbf7c1301c1cd234308e49a6ca7e8
Akash Anup <akashanup.1993@hotmail.com>
2022-04-23 13:18:24 +0530

    287.FindTheDuplicateNumber

--- 287.FindTheDuplicateNumber/solution.py
+++ 287.FindTheDuplicateNumber/solution.py
@@ -1,15 +1,49 @@
-class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        slow, fast = nums[0], nums[0]
-        while True:
-            slow = nums[slow]
-            fast = nums[nums[fast]]
-            if slow == fast:
-                break
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
         
-        fast = nums[0]
-        while slow != fast:
-            fast = nums[fast]
-            slow = nums[slow]
+        :type root: TreeNode
+        :rtype: str
+        """
+
+        def dfs(root):
+            if not root:
+                ans.append('#')
+                return
+            ans.append(str(root.val))
+            dfs(root.left)
+            dfs(root.right)
+
+        ans = []
+        dfs(root)
+        return ' '.join(ans)
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
         
-        return slow
+        :type data: str
+        :rtype: TreeNode
+        """
+
+        def dfs(index):
+            if data[index] == '#': return None, index
+            root = TreeNode(int(data[index]))
+            root.left, index = dfs(index + 1)
+            root.right, index = dfs(index + 1)
+            return root, index
+
+        data = data.split()
+        return dfs(0)[0]
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
