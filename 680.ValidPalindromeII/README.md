74ba5b10bdd8a048cd8592be96e3d73b85697770
Akash Anup <akashanup.1993@hotmail.com>
2022-05-31 17:25:33 +0530

    680.ValidPalindromeII

---
+++ 680.ValidPalindromeII/README.md
@@ -0,0 +1,28 @@
+# 680. Valid Palindrome II
+
+Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+### Example 1
+```sh
+Input: s = "aba"
+Output: true
+```
+
+### Example 2
+```sh
+Input: s = "abca"
+Output: true
+Explanation: You could delete the character 'c'.
+```
+
+### Example 3
+```sh
+Input: s = "abc"
+Output: false
+```
+
+### Constraints
+```sh
+1 <= s.length <= 10^5
+s consists of lowercase English letters.
+```
---
+++ 680.ValidPalindromeII/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def recur(self, s, start, end, deleted):
+        if deleted > 1:
+            return False
+        if start >= end:
+            return True
+        while start < end:
+            if s[start] == s[end]:
+                start += 1
+                end -= 1
+            else:
+                return self.recur(s, start, end-1, deleted+1) or self.recur(s, start+1, end, deleted+1)
+        return True
+
+    def validPalindrome(self, s: str) -> bool:
+        return self.recur(s, 0, len(s)-1, 0)
+    
